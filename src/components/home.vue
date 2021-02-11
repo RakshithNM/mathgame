c78ab0e61885c63986c426b17aa9af3f2a3b8243
RakshithNM <rakshith_bellare@outlook.com>
2021-02-11 23:44:09 +0530

    Adding missing home page

---
+++ src/components/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {
+  data() {
+    return {
+      title: "MATHEMATICS GAME",
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+h1 {
+  color: blue;
+  text-align: center;
+}
+</style>
